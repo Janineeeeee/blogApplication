7ce460035623ad42b29d8cca69686f56cb499c84
janineyabanprieto <janineyabanprieto@gmail.com>
2024-10-21 20:44:07 +0800

    Added link

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const corsOptions = {
-	origin: ['http://localhost:3000'],
+	origin: ['http://localhost:3000', 'https://swiftink.onrender.com'],
 	credentials: true,
 	optionsSuccessStatus: 200
 };
